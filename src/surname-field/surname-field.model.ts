15062e7f913aeb25e7e312cc8a1e277db82220d1
Cristiam Sosa <csosaur@gmail.com>
2020-06-30 00:03:18 +0200

    Add new generator

---
+++ src/surname-field/surname-field.model.ts
@@ -0,0 +1,5 @@
+import { FieldModel } from '../core/schema.model';
+
+export interface SurnameFieldModel extends FieldModel {
+  source?: string[];
+}
---
+++ test/surname-field/surname-field.generator.test.ts
@@ -0,0 +1,38 @@
+import { expect } from '@oclif/test';
+import { it } from 'mocha';
+import { SurnameFieldGenerator } from '../../src/surname-field/surname-field.generator';
+import { SurnameFieldSource } from '../../src/surname-field/surname-field.source';
+
+describe('SurnameFieldGenerator', () => {
+  const generator = new SurnameFieldGenerator();
+  const type = generator.type
+
+  it('should be generate any name from default source', () => {
+    const name = 'last name'
+
+    const result = generator.generate({type, name});
+
+    expect(result.key).equal(name)
+    expect(SurnameFieldSource.source).contains(result.value)
+  })
+
+  it('should be generate any name from specific source', () => {
+    const source = ['Sosa', 'Gil']
+    const name = 'last name'
+    const result = generator.generate({type, name, source});
+
+    expect(result.key).equal(name)
+    expect(source).contains(result.value)
+  })
+
+  it('should generate null value, 1 of 4', () => {
+    const name = 'last name'
+    const isNullable = true
+
+    const results = Array.from(Array(10).keys())
+      .map(() => generator.generate({type, name, isNullable}))
+
+    expect(results.every(r => r.key === name)).true
+    expect(results.some(r => !r.value)).true
+  })
+})
