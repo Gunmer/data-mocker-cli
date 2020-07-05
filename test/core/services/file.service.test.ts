15451b7f7c98e2a59dca6ae319374fdd15226067
Cristiam Sosa <csosaur@gmail.com>
2020-07-05 17:25:19 +0200

    Fix test

--- test/core/services/file.service.test.ts
+++ test/core/services/file.service.test.ts
@@ -1,19 +1,26 @@
-import { expect } from '@oclif/test';
-import { FileNotFoundException } from '../../../src/core/exceptions/file-not-found.exception';
-import { FileService } from '../../../src/core/services/file.service';
+import { expect, test } from '@oclif/test'
+import * as fs from 'fs'
+import { FileNotFoundException } from '../../../src/core/exceptions/file-not-found.exception'
+import { FileService } from '../../../src/core/services/file.service'
 
 describe('FileService', () => {
   const service = new FileService()
 
-  it('should create error.json file', () => {
-    const result = service.writeErrorLog(new FileNotFoundException())
+  test
+    .stub(fs, 'writeFileSync', () => {
+    })
+    .it('should create error.json file', () => {
+      const result = service.writeErrorLog(new FileNotFoundException())
 
-    expect(result).to.contain('error.json')
-  })
+      expect(result).to.contain('error.json')
+    })
 
-  it('should create result.sql file', () => {
-    const result = service.writeSql([])
+  test
+    .stub(fs, 'writeFileSync', () => {
+    })
+    .it('should create result.sql file', () => {
+      const result = service.writeSql([])
 
-    expect(result).to.contain('result.sql')
-  })
+      expect(result).to.contain('result.sql')
+    })
 })
